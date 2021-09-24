495afe5e328b25f23bf5c649d356a5ba3d1b0889
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-09-24 16:07:46 +0200

    set env vars

--- typesense-client/lib/utils.js
+++ typesense-client/lib/utils.js
@@ -1,11 +1,11 @@
 export function assembleTypesenseServerConfig() {
   let TYPESENSE_SERVER_CONFIG = {
-    apiKey: process.env.NEXT_PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY, // Be sure to use an API key that only allows searches, in production
+    apiKey: process.env.TYPESENSE_SEARCH_ONLY_API_KEY, // Be sure to use an API key that only allows searches, in production
     nodes: [
       {
-        host: process.env.NEXT_PUBLIC_TYPESENSE_HOST,
-        port: process.env.NEXT_PUBLIC_TYPESENSE_PORT,
-        protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
+        host: process.env.TYPESENSE_HOST,
+        port: process.env.TYPESENSE_PORT,
+        protocol: process.env.TYPESENSE_PROTOCOL,
       },
     ],
     numRetries: 8,
@@ -15,24 +15,24 @@ export function assembleTypesenseServerConfig() {
   if (process.env[`TYPESENSE_HOST_2`]) {
     TYPESENSE_SERVER_CONFIG.nodes.push({
       host: process.env[`TYPESENSE_HOST_2`],
-      port: process.env.NEXT_PUBLIC_TYPESENSE_PORT,
-      protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
+      port: process.env.TYPESENSE_PORT,
+      protocol: process.env.TYPESENSE_PROTOCOL,
     });
   }
 
   if (process.env[`TYPESENSE_HOST_3`]) {
     TYPESENSE_SERVER_CONFIG.nodes.push({
       host: process.env[`TYPESENSE_HOST_3`],
-      port: process.env.NEXT_PUBLIC_TYPESENSE_PORT,
-      protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
+      port: process.env.TYPESENSE_PORT,
+      protocol: process.env.TYPESENSE_PROTOCOL,
     });
   }
 
   if (process.env[`TYPESENSE_HOST_NEAREST`]) {
     TYPESENSE_SERVER_CONFIG['nearestNode'] = {
       host: process.env[`TYPESENSE_HOST_NEAREST`],
-      port: process.env.NEXT_PUBLIC_TYPESENSE_PORT,
-      protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
+      port: process.env.TYPESENSE_PORT,
+      protocol: process.env.TYPESENSE_PROTOCOL,
     };
   }
 
--- typesense-client/scripts/populateTypesenseIndex.js
+++ typesense-client/scripts/populateTypesenseIndex.js
@@ -7,12 +7,12 @@ module.exports = (async () => {
   const typesense = new Typesense.Client({
     nodes: [
       {
-        host: process.env.NEXT_PUBLIC_TYPESENSE_HOST,
-        port: process.env.NEXT_PUBLIC_TYPESENSE_PORT,
-        protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL,
+        host: process.env.TYPESENSE_HOST,
+        port: process.env.TYPESENSE_PORT,
+        protocol: process.env.TYPESENSE_PROTOCOL,
       },
     ],
-    apiKey: process.env.NEXT_PUBLIC_TYPESENSE_ADMIN_API_KEY,
+    apiKey: process.env.TYPESENSE_ADMIN_API_KEY,
   });
 
   const schema = {
